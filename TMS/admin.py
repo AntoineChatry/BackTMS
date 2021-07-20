28cf9a7064b8ef0057bbbfa6698dad39aed3b288
AntoineChatry <antoine.chatry78@orange.fr>
2021-07-20 15:11:01 +0200

    Update admin.py

--- TMS/admin.py
+++ TMS/admin.py
@@ -3,7 +3,8 @@ from .models import TMS
 
 class TmsAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed', 'places')
-
+    
 # Register your models here.
+admin.site.site_header = "TrackMySeat Administration"
 
 admin.site.register(TMS, TmsAdmin)
